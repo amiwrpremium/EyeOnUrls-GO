d76e7d944b7634100e0300d9264c0b5ca022aa7c
AMiWR <amiwrpremium@gmail.com>
2024-02-14 17:40:29 +0330

    🗃️ Make ShortUrl unique

--- models/urls.go
+++ models/urls.go
@@ -5,19 +5,22 @@ import (
 	"time"
 )
 
+// Url represents the model for a shortened URL
 type Url struct {
 	ID          int       `json:"id" gorm:"primary_key"`
 	OriginalUrl string    `json:"original_url"`
-	ShortUrl    string    `json:"short_url"`
+	ShortUrl    string    `json:"short_url" gorm:"unique"`
 	CreatedAt   time.Time `json:"created_at" gorm:"autoCreateTime"`
 	UpdatedAt   time.Time `json:"updated_at" gorm:"autoUpdateTime"`
 	ViewCount   int       `json:"view_count" gorm:"default:0"`
 }
 
+// CreateUrlInput represents the input for the CreateUrl function
 type CreateUrlInput struct {
 	OriginalUrl string `json:"link" binding:"required"`
 }
 
+// GenerateShortUrl generates a random short URL
 func GenerateShortUrl() string {
 	const charset = "abcdefghijklmnopqrstuvwxyz" +
 		"ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
